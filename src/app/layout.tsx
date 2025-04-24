00d2eec99884a1e4ffd2ffdfb33b282eee080758
Akanksha <akanksha@auspicioussoft.com>
2025-04-24 17:57:32 +0530

    commit changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,20 +35,7 @@ export default function RootLayout({
     </html>
   );
 
-
-
-
   
 }
-// export function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <SidebarProvider>
-//       <AppSidebar />
-//       <main>
-//         <SidebarTrigger />
-//         {children}
-//       </main>
-//     </SidebarProvider>
-//   )
-// }
+
 
